4a67dac018f72f17625193b7d93891a37880bd20
kongxiangshu <kongxiangshu@baicheng.com>
2016-08-26 09:15:40 +0800

    update

--- src/cn/azir/eUtils/MainActivity.java
+++ src/cn/azir/eUtils/MainActivity.java
@@ -1,14 +1,23 @@
 package cn.azir.eUtils;
 
 
+import java.io.File;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.Set;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,17 +38,45 @@ public class MainActivity extends Activity {
 		
 		NTInject.inject(this);
 		
-		
 		mText.setText("hfldkjakls");
 		
 		
+		
 		helloListView.setAdapter(new MyAdapter());
+		
+		
+		try {
+			Class<?> cls = Class.forName("android.content.pm.PackageParser");
+			Object obj = cls.newInstance();
+			Method parsePackage = cls.getDeclaredMethod("parsePackage", File.class,String.class,DisplayMetrics.class,Integer.class);
+			
+			System.out.println(parsePackage);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Exception -- " + e.getMessage());
+		}
+		
+		
+		
 		
 	}
 	
 	
 	protected void onClickEvent(View view){
 		Toast.makeText(getApplicationContext(), "onClickEvent", 0).show();
+		
+		try {
+			Class<?> cls = Class.forName("android.os.ServiceManager");
+			Object obj = cls.newInstance();
+			Method addService = cls.getDeclaredMethod("addService", String.class,IBinder.class);
+			addService.setAccessible(true);
+//			addService.invoke(obj, "hello",new NtService());
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("exception : " +e.getLocalizedMessage());
+		}
+		
 	}
 	
 	protected boolean longEvent(View v){
---
+++ src/cn/azir/eUtils/aidl/IComputer.aidl
@@ -0,0 +1,5 @@
+package cn.azir.eUtils.aidl;
+
+interface IComputer {
+	int com(int a,int b);
+}
